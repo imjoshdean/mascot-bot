7d4924da2cec6ed80aed642c5265dca9cbc048a2
imjoshdean <imjoshdean@me.com>
2016-10-21 00:42:54 -0500

    Add introduction behavior

---
+++ src/behaviors/introductions/introductions.js
@@ -0,0 +1,32 @@
+import Behavior from '../behavior.js';
+import messages from './messages.json';
+
+class Introductions extends Behavior {
+  constructor(settings = {}) {
+    settings.name = 'Introductions';
+
+    super(settings);
+  }
+
+  initialize(bot) {
+    super.initialize(bot);
+    let promise = Promise.resolve();
+
+    Promise.all([bot.getUser('imjoshdean'), bot.getUser('sheva')]).then((users) => {
+      const [josh, sheva] = users,
+        joshTag = `<@${josh.id}|${josh.name}>`,
+        shevaTag = `<@${sheva.id}|${sheva.name}>`;
+
+      messages.messages.forEach((message) => {
+        promise = promise.then(() => {
+          const text = message.text.replace('@imjoshdean', joshTag).replace('@sheva', shevaTag);
+          return bot.say('#drop-the-beatz', text, message.options);
+        });
+      });
+    });
+
+    return promise;
+  }
+}
+
+export default Introductions;
