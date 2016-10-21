08fa478b9dba6dfd79f88350e221966e8e8e9a26
imjoshdean <imjoshdean@me.com>
2016-10-21 11:41:00 -0500

    Tweak random channel with new suffixes.

--- src/behaviors/random-channel/random-channel.js
+++ src/behaviors/random-channel/random-channel.js
@@ -1,4 +1,12 @@
 import Behavior from '../behavior.js';
+import _sample from 'lodash/sample';
+
+// Series of ways to end the random channel statement.
+const CHECK_IT_OUT = [
+  'Check it out, yo!',
+  'Join in on the fun!',
+  'Come see what you\'re missing!'
+];
 
 class RandomChannel extends Behavior {
   constructor(settings = {}) {
@@ -10,9 +18,7 @@ class RandomChannel extends Behavior {
   initialize(bot) {
     super.initialize(bot);
 
-    this.scheduleJob('* * * * *', () => {
-      bot.say(this.settings.sayInChannel, 'Once a minute, I\'ll say a random room name');
-
+    this.scheduleJob('0 9 * * *', () => {
       this.sayRandomChannel(bot);
     });
   }
@@ -28,12 +34,13 @@ class RandomChannel extends Behavior {
       } while (['all-staff', 'announcements'].includes(randomChannel.name));
 
       const purposeless = '[no purpose set, but it\'s probably pretty good anyway]',
+        checkIt = _sample(CHECK_IT_OUT),
         channelPurpose = randomChannel.purpose.value === '' ? purposeless
             : randomChannel.purpose.value,
         period = ['.', '!', '?'].includes(channelPurpose.slice(-1)) ? '' : '.',
         randomChannelMessage = `The random channel of the day is ` +
           `<#${randomChannel.id}|${randomChannel.name}>: ` +
-          `${channelPurpose}${period} Check it out, yo!`;
+          `${channelPurpose}${period} ${checkIt}`;
 
       bot.say(this.settings.sayInChannel, randomChannelMessage, {
         icon_emoji: ':slack:'
