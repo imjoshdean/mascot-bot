efb5ad3a1a081648067e2c84528d2645959149fb
imjoshdean <imjoshdean@me.com>
2016-10-21 11:41:18 -0500

    Update Beatz Bot behaviors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import MascotBot from './bots/mascot-bot.js';
 import RandomChannel from './behaviors/random-channel/random-channel.js';
 import DaysUntil from './behaviors/days-until/days-until.js';
+import Introductions from './behaviors/introductions/introductions.js';
 
 const name = process.env.NAME || 'Beatz Bot',
   beatzBot = new MascotBot({
     name,
     behaviors: [
+      Introductions,
       {
         behavior: DaysUntil,
         settings: {
-          conDate: '10/19/2016',
+          conDate: '08/11/2017',
           sayInChannel: '#drop-the-beatz',
           isPubic: false
         }
-      }/*,
+      },
       {
         behavior: RandomChannel,
         settings: {
           sayInChannel: '#drop-the-beatz'
         }
-      }*/
+      }
     ]
   });
 
