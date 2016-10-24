5ee6343937cfcf2d337592d090cec51be94527a8
imjoshdean <imjoshdean@me.com>
2016-10-24 10:27:11 -0500

    Fix typo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import MascotBot from './bots/mascot-bot.js';
 import RandomChannel from './behaviors/random-channel/random-channel.js';
 import DaysUntil from './behaviors/days-until/days-until.js';
-import Introductions from './behaviors/introductions/introductions.js';
 
 const name = process.env.NAME || 'Beatz Bot',
   beatzBot = new MascotBot({
     name,
     behaviors: [
-      Introductions,
       {
         behavior: DaysUntil,
         settings: {
           conDate: '8/11/2017',
           sayInChannel: '#all-staff',
-          isPubic: true
+          isPublic: true
         }
       },
       {
