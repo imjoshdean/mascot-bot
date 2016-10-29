073bba8364ad54f8dc184ff4d5583716a42792ce
imjoshdean <imjoshdean@me.com>
2016-10-29 18:38:14 -0500

    Enable RollTheDice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import MascotBot from './bots/mascot-bot.js';
 import RandomChannel from './behaviors/random-channel/random-channel.js';
 import DaysUntil from './behaviors/days-until/days-until.js';
+import RollTheDice from './behaviors/roll-the-dice/roll-the-dice.js';
 
 const name = process.env.NAME || 'Beatz Bot',
   beatzBot = new MascotBot({
     name,
     behaviors: [
+      RollTheDice,
       {
         behavior: DaysUntil,
         settings: {
