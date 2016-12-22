1e353b27dc92f7e79fdbc7edfeb1d4f08f8bae0a
imjoshdean <imjoshdean@me.com>
2016-12-21 18:55:48 -0600

    Finish adding karma for things

--- src/behaviors/karma/karma.js
+++ src/behaviors/karma/karma.js
@@ -3,7 +3,9 @@ import Karma from './models/karma.js';
 
 const USER_KARMA_REGEX = /<@(\w+)(?:\|\w+)?>(?:[\)s\:]*)(\+\+|\-\-)(?:\s?(?:#|\/\/)\s?((?:[\s\S])+))?/gi,
   USER_REGEX = /<@(\w+)>/gi,
-  THING_REGEX = /((?:\w+)|["|“|”](?:[\s\S]+)["|“|”])(\+\+|\-\-)(?:\s?(?:#|\/\/)\s?((?:[\s\S])+))?/gi;
+  THING_KARMA_REGEX = /((?:\w+)|["|“|”](?:[\s\S]+)["|“|”])(\+\+|\-\-)(?:\s?(?:#|\/\/)\s?((?:[\s\S])+))?/gi,
+  THING_REGEX = /!explain ((?:\w+)|["|“|”](?:[\s\S]+)["|“|”])/gi,
+  LIST_REGEX = /^!(?:top|bottom) (thing|person)/gi;
 
 class KarmaBehavior extends Behavior {
   constructor(settings) {
@@ -18,8 +20,15 @@ class KarmaBehavior extends Behavior {
     });
 
     this.commands.push({
-      tag: 'list',
-      description: 'I\'ll list the top 10 karma users when you say `!list top` and the bottom 10 when you say `!list bottom`'
+      tag: 'top',
+      description: 'I\'ll list the top 10 karma for people and things. If you want just one or the '
+        + 'other, try `!top person` or `!top thing`'
+    });
+
+    this.commands.push({
+      tag: 'bottom',
+      description: 'I\'ll list the bottom 10 karma for people and things. If you want just one or the '
+        + 'other, try `!bottom person` or `!bottom thing`'
     });
   }
 
@@ -31,6 +40,8 @@ class KarmaBehavior extends Behavior {
     if (messageData.text && messageData.text.match(USER_KARMA_REGEX)) {
       const [, userId, type, reason] = USER_KARMA_REGEX.exec(messageData.text),
         channel = messageData.channel;
+
+      USER_KARMA_REGEX.lastIndex = 0;
 
       // Trying to give yourself karma? tsk tsk. Not gonna fly.
       if (userId === messageData.user) {
@@ -73,13 +84,15 @@ class KarmaBehavior extends Behavior {
         });
       });
     }
-    else if (messageData.text && messageData.text.match(THING_REGEX)) {
+    else if (messageData.text && messageData.text.match(THING_KARMA_REGEX)) {
       if (messageData.text.includes('karma has changed to')) {
         return;
       }
 
-      const [, thing, type, reason] = THING_REGEX.exec(messageData.text),
+      const [, thing, type, reason] = THING_KARMA_REGEX.exec(messageData.text),
         channel = messageData.channel;
+
+      THING_KARMA_REGEX.lastIndex = 0;
 
       this._getKarmaAndThing(thing).then(data => {
         const thing = data.thing,
@@ -105,69 +118,86 @@ class KarmaBehavior extends Behavior {
   execute(command, message, channel) {
     switch (command) {
     case 'explain':
-      this.getUserKarma(message, channel);
+      this.explainKarma(message, channel);
       break;
-    case 'list':
-      this.listKarma(message, channel);
+    case 'top':
+      this.listKarma(message, channel, true);
+      break;
+    case 'bottom':
+      this.listKarma(message, channel, false);
       break;
     default:
       break;
     }
   }
 
-  listKarma(message, channel) {
-    if (message.startsWith('!list top')) {
-      Karma.list('desc').then(karmaList => {
-        let karmaMessage = `The people with the most karma:\n\n`;
+  listKarma(message, channel, isTop = true) {
+    const [, entity] = LIST_REGEX.exec(message) || [];
+    let karmaMessage = `The ${entity ? `${entity}s` : 'people and things'} with the ${isTop ? 'most' : 'least'} karma:\n\n`;
 
-        karmaList.forEach(karma => {
+
+    LIST_REGEX.lastIndex = 0;
+
+    Karma.list(isTop ? 'desc' : 'asc', 10, entity).then(karmaList => {
+      karmaList.forEach(karma => {
+        if (karma.name) {
+          karmaMessage += `${karma.karma} ${karma.name}\n`;
+        }
+        else {
           karmaMessage += `${karma.karma} <@${karma.entityName}>\n`;
-        });
+        }
+      });
 
-        this.bot.postMessage(channel, karmaMessage, {
-          icon_emoji: ':karma:'
-        });
+      this.bot.postMessage(channel, karmaMessage, {
+        icon_emoji: `:${isTop ? 'karma' : 'discentia'}:`
+      });
+    });
+  }
+
+  explainKarma(message, channel) {
+    const [, userId] = USER_REGEX.exec(message) || [];
+    const [, thing] = THING_REGEX.exec(message) || [];
+
+    USER_REGEX.lastIndex = 0;
+    THING_REGEX.lastIndex = 0;
+
+    if (userId) {
+      this.bot.users = undefined;
+      this._getKarmaAndUser(userId).then(data => {
+        const user = data.user,
+          karma = data.karma,
+          positive = karma.sample(5, 'positive').map(reason => reason.reason).join('; '),
+          negative = karma.sample(5, 'negative').map(reason => reason.reason).join('; ');
+
+        this._postKarma(channel, `<@${user.id}|${user.name}>`, karma, positive, negative);
       });
     }
-    else if (message.startsWith('!list bottom')) {
-      Karma.list('asc').then(karmaList => {
-        let karmaMessage = `The people with the least karma:\n\n`;
+    else if (thing) {
+      this._getKarmaAndThing(thing).then(data => {
+        const thing = data.thing,
+          karma = data.karma,
+          positive = karma.sample(5, 'positive').map(reason => reason.reason).join('; '),
+          negative = karma.sample(5, 'negative').map(reason => reason.reason).join('; ');
 
-        karmaList.forEach(karma => {
-          karmaMessage += `${karma.karma} <@${karma.entityName}>\n`;
-        });
-
-        this.bot.postMessage(channel, karmaMessage, {
-          icon_emoji: ':discentia:'
-        });
+        this._postKarma(channel, `"${thing}"`, karma, positive, negative);
       });
     }
   }
 
-  getUserKarma(message, channel) {
-    const [, userId] = USER_REGEX.exec(message);
+  _postKarma(channel, thing, karma, positive, negative) {
+    let karmaMessage = `${thing} has ${karma.karma} karma. The highest it's ` +
+      `ever been was ${karma.highest} and the lowest it's ever been was ${karma.lowest}.\n\n`;
 
-    this.bot.users = undefined;
-    this._getKarmaAndUser(userId).then(data => {
-      const user = data.user,
-        karma = data.karma,
-        positive = karma.sample(5, 'positive').map(reason => reason.reason).join('; '),
-        negative = karma.sample(5, 'negative').map(reason => reason.reason).join('; ');
+    if (positive) {
+      karmaMessage += `Positive: ${positive}\n`;
+    }
 
-      let karmaMessage = `<@${user.id}|${user.name}> has ${karma.karma} karma. The highest it's ` +
-        `ever been was ${karma.highest} and the lowest it's ever been was ${karma.lowest}.\n\n`;
+    if (negative) {
+      karmaMessage += `Negative: ${negative}\n`;
+    }
 
-      if (positive) {
-        karmaMessage += `Positive: ${positive}\n`;
-      }
-
-      if (negative) {
-        karmaMessage += `Negative: ${negative}\n`;
-      }
-
-      this.bot.postMessage(channel, karmaMessage, {
-        icon_emoji: ':karma:'
-      });
+    this.bot.postMessage(channel, karmaMessage, {
+      icon_emoji: ':karma:'
     });
   }
 
@@ -188,8 +218,8 @@ class KarmaBehavior extends Behavior {
 
   _getKarmaAndThing(thing) {
     // There are too many different kinds of double quotes...
-    thing = thing.match(/^["|“|”]/) ? thing.slice(1, -1) : thing;
-    const sanitized = thing.replace(' ', '_').replace(/\W/g, '').toLowerCase();
+    thing = Karma.stripQuotes(thing);
+    const sanitized = Karma.sanitize(thing);
 
     return new Promise(resolve => {
       Karma.findOrCreate({
