64dd173d5ec9dc5a4211e17c059d1cd410bdeaa8
imjoshdean <imjoshdean@me.com>
2017-06-08 15:01:43 -0500

    Add whitespace

--- src/behaviors/clap/clap.js
+++ src/behaviors/clap/clap.js
@@ -15,7 +15,7 @@ class RollTheDice extends Behavior {
   execute(command, message, channel, data) {
     const parsedMessage = this.parseMessage(message);
 
-    this.bot.postMessage(channel, parsedMessage.join(':clap:'), {
+    this.bot.postMessage(channel, parsedMessage.join(' :clap: '), {
       icon_emoji: ':clap:',
       thread_ts: data.thread_ts
     });
