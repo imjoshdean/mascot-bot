b028be9324165914e1d9fb6fe124e07bd7b5eb5c
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-21 14:05:47 -0500

    Revert changes to original ((rtd fix test))
    UGH, Reverted to baseline

--- src/behaviors/roll-the-dice/roll-the-dice.js
+++ src/behaviors/roll-the-dice/roll-the-dice.js
@@ -19,7 +19,7 @@ class RollTheDice extends Behavior {
     let results = '',
       sum = 0;
 
-    if (roll.number > "100" || roll.sides > "100") {
+    if (roll.number > 100 || roll.sides > 100) {
       this.bot.postMessage(channel, `Don't be a dick, no more than 100 dice or 100 sides`, {
         icon_emoji: ':game_die:',
         thread_ts: data.thread_ts
@@ -27,37 +27,6 @@ class RollTheDice extends Behavior {
 
       roll.number = Math.min(100, roll.number);
       roll.sides = Math.min(100, roll.sides);
-    }
-
-    for (let i = 0; i < roll.number; i++) {
-      rolls.push(Math.ceil(Math.random() * roll.sides));
-    }
-
-    results = rolls.join(', ');
-
-    sum = rolls.reduce((a, b) => a + b);
-    results += ` (${sum})`;
-
-    this.bot.postMessage(channel, `You rolled ${results}.`, {
-      icon_emoji: ':game_die:',
-      thread_ts: data.thread_ts
-    });
-  }
-
-execute(command, message, channel, data) {
-    const roll = this.parseRoll(message),
-      rolls = [];
-    let results = '',
-      sum = 0;
-
-    if (roll.number < "1" || roll.sides < "1") {
-      this.bot.postMessage(channel, `(Please enter number of dice being rolled and Number of Sides like "!rtd 1d5)`, {
-        icon_emoji: ':game_die:',
-        thread_ts: data.thread_ts
-      });
-
-      roll.number = Math.min(1, roll.number);
-      roll.sides = Math.min(5, roll.sides);
     }
 
     for (let i = 0; i < roll.number; i++) {
