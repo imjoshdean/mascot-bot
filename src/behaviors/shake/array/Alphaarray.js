6fac31be37ba809deec11f80021a8d71a9533913
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-22 16:46:23 -0500

    Array
    alpha array

---
+++ src/behaviors/shake/array/Alphaarray.js
@@ -0,0 +1,36 @@
+var alpha = [
+    {name: "a", emoji: ":sh-a:"}
+    {name: "b", emoji: ":sh-b:"}
+    {name: "c", emoji: ":sh-c:"}
+    {name: "d", emoji: ":sh-d:"}
+    {name: "e", emoji: ":sh-e:"}
+    {name: "f", emoji: ":sh-f:"}
+    {name: "g", emoji: ":sh-g:"}
+    {name: "h", emoji: ":sh-h:"}
+    {name: "i", emoji: ":sh-i:"}
+    {name: "j", emoji: ":sh-j:"}
+    {name: "k", emoji: ":sh-k:"}
+    {name: "l", emoji: ":sh-l:"}
+    {name: "m", emoji: ":sh-m:"}
+    {name: "n", emoji: ":sh-n:"}
+    {name: "o", emoji: ":sh-o:"}
+    {name: "p", emoji: ":sh-p:"}
+    {name: "q", emoji: ":sh-q:"}
+    {name: "e", emoji: ":sh-r:"}
+    {name: "s", emoji: ":sh-s:"}
+    {name: "t", emoji: ":sh-t:"}
+    {name: "u", emoji: ":sh-u:"}
+    {name: "v", emoji: ":sh-v:"}
+    {name: "w", emoji: ":sh-w:"}
+    {name: "x", emoji: ":sh-x:"}
+    {name: "y", emoji: ":sh-y:"}
+    {name: "z", emoji: ":sh-z:"}
+];
+
+var alphaEmoji = alpha.map(function(x){
+  return x.name + x.emoji;
+
+});
+
+console.log(alphaEmoji[y])
+ 
