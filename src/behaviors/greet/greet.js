f36f0fac114a453e36ba42e39224b2a5c8d2f1f4
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-22 07:16:39 -0500

    space put back
    Turns out we might need it?

--- src/behaviors/greet/greet.js
+++ src/behaviors/greet/greet.js
@@ -2,7 +2,7 @@ import Behavior from '../behavior.js';
 
 export default class GreetBehavior extends Behavior {
   constructor(settings = {}) {
-    settings.name = 'GreetBehavior';
+    settings.name = 'Greet Behavior';
 
     super(settings);
 
