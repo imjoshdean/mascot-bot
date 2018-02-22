7dbac1de9f668a80d4859cd1c383698d1470b0b2
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-22 07:56:52 -0500

    Cool
    nothing to see here

--- src/behaviors/impossible/impossible.js
+++ src/behaviors/impossible/impossible.js
@@ -6,13 +6,13 @@ export default class GreetBehavior extends Behavior {
 
     super(settings);
 
-    this.addCommand('dotheimpossible', 'Say hello back whenever someone types !dotheimpossible');
+    this.addCommand('dotheimpossible', 'JUST WHO THE :sh-h::sh-e:sh-l::sh-l: *DO YOU THINK I AM?!*');
   }
 
   execute(command, message, channel, data) {
     switch (command) {
     case 'dotheimpossible':
-      this.bot.postMessage(channel, `<@${data.user}>:sh-r::sh-o::sh-w::sh-r::sh-o::sh-w::blank::sh-f::sh-i::sh-g::sh-h::sh-t::blank::sh-t::sh-h::sh-e::blank::sh-p::sh-o::sh-w::sh-e::sh-r:`, {
+      this.bot.postMessage(channel, `<@${data.user}>:sh-r::sh-o::sh-w::sh-r::sh-o::sh-w::blank::sh-f::sh-i::sh-g::sh-h::sh-t::blank::sh-t::sh-h::sh-e::blank::sh-p::sh-o::sh-w::sh-a::sh-h:`, {
         icon_emoji: ':rowrowHoof_beatz:'
       });
       break;
