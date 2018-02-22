d24b152763f3f169318eb6347d0d26fd485cc56a
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-22 07:41:36 -0500

    rd4
    killme

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
- import RandomChannel from './behaviors/random-channel/random-channel.js';
- import DaysUntil from './behaviors/days-until/days-until.js';
- import RollTheDice from './behaviors/roll-the-dice/roll-the-dice.js';
- import Birthdays from './behaviors/birthdays/birthdays.js';
- import Karma from './behaviors/karma/karma.js';
- import Eventbrite from './behaviors/eventbrite/eventbrite.js';
- import Clap from './behaviors/clap/clap.js';
- import settings from './settings.json';
- 
- 
- const mascot = new MascotBot({
-   ...settings.bot,
-   behaviors: [
-     Clap,
-     ExplainBehaviors,
-     {
-       behavior: Birthdays,
-       settings: settings.birthdays
-     },
-     RollTheDice,
-      {
-        behavior: DaysUntil,
-        settings: {
-          conDate: '7/28/2018',
-          maxDays: 398,
-          sayInChannel: '#all-staff',
-          isPublic: true
-       }
-     },
-     {
-       behavior: RandomChannel,
-       settings: {
-         sayInChannel: '#all-staff'
-       }
-     },
-     Karma,
-     {
-       behavior: Eventbrite,
-       settings: settings.eventbrite
-     }
-   ]
- });
- 
- mascot.launch();
+import MascotBot from './bots/mascot-bot.js';
+import ExplainBehaviors from './behaviors/explain-behaviors/explain-behaviors.js';
+import RandomChannel from './behaviors/random-channel/random-channel.js';
+import DaysUntil from './behaviors/days-until/days-until.js';
+import RollTheDice from './behaviors/roll-the-dice/roll-the-dice.js';
+import Birthdays from './behaviors/birthdays/birthdays.js';
+import Karma from './behaviors/karma/karma.js';
+import Eventbrite from './behaviors/eventbrite/eventbrite.js';
+import Clap from './behaviors/clap/clap.js';
+import settings from './settings.json';
+
+
+const mascot = new MascotBot({
+  ...settings.bot,
+  behaviors: [
+    Clap,
+    ExplainBehaviors,
+    {
+      behavior: Birthdays,
+      settings: settings.birthdays
+    },
+    RollTheDice,
+    {
+      behavior: DaysUntil,
+      settings: {
+        conDate: '8/11/2017',
+        maxDays: 398,
+        sayInChannel: '#all-staff',
+        isPublic: true
+      }
+    },
+    {
+      behavior: RandomChannel,
+      settings: {
+        sayInChannel: '#all-staff'
+      }
+    },
+    Karma,
+    {
+      behavior: Eventbrite,
+      settings: settings.eventbrite
+    }
+  ]
+});
+
+mascot.launch();
