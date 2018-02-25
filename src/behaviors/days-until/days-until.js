261334df58faf379bb5d3f3cdb75fc7f99d365dd
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-25 12:25:54 -0500

    exp. test
    1pm Monday, Sunday(test)

--- src/behaviors/days-until/days-until.js
+++ src/behaviors/days-until/days-until.js
@@ -18,7 +18,7 @@ class DaysUntil extends Behavior {
   initialize(bot) {
     super.initialize(bot);
 
-    this.scheduleJob('0 13 * * 1-7', () => {
+    this.scheduleJob('0 13 * * 1,7', () => {
       this.updateTopic(bot);
     });
   }
