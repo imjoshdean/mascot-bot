74ea02f51d77e70ecdede294e3f8b2a7d9b607bf
chibishibe <35297107+chibishibe@users.noreply.github.com>
2018-02-25 20:58:47 -0500

    Update random-channel.js

--- src/behaviors/random-channel/random-channel.js
+++ src/behaviors/random-channel/random-channel.js
@@ -23,7 +23,7 @@ class RandomChannel extends Behavior {
   initialize(bot) {
     super.initialize(bot);
 
-    this.scheduleJob('00 00 14 * * 1-7', () => {
+    this.scheduleJob('1 21 * * *', () => {
       this.sayRandomChannel(bot);
     });
   }
